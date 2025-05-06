a34108d4c4b5596f0e799d6e514f9fa9b5d9d1bc
Добровольская Маша <dobrbobr89@gmail.com>
2025-05-06 22:55:48 +0400

    Task1 correct 1

--- src/Author.java
+++ src/Author.java
@@ -2,19 +2,21 @@ public class Author {
     private String authorName;
     private String authorSurname;
 
-    public Author(String authorName, String authorSurname){
+    public Author(String authorName, String authorSurname) {
         this.authorName = authorName;
         this.authorSurname = authorSurname;
     }
-    public String getAuthorName(){
+
+    public String getAuthorName() {
         return this.authorName;
     }
-    public String getAuthorSurname(){
+
+    public String getAuthorSurname() {
         return this.authorSurname;
     }
 
     @Override
     public String toString() {
-        return  authorName + " " + authorSurname ;
+        return authorName + " " + authorSurname;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Author author1 = new Author("Николай", "Гололь");
 
-        Book book1 = new Book("Мертвые души", author1,  1842);
+        Book book1 = new Book("Мертвые души", author1, 1842);
         System.out.println();
         System.out.println("book1.getAuthor() = " + book1.getAuthor());
         System.out.println("book1.getName() = " + book1.getName());
